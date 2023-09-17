889bf90903d296f35cf63565deb2f57680fbd961
Ameya Jadhav <jadhavameyak@gmail.com>
2023-09-17 01:20:31 -0400

    fix

--- backend/test_scene.py
+++ backend/test_scene.py
@@ -2,6 +2,4 @@ from scenedetect import detect, AdaptiveDetector, split_video_ffmpeg
 
 
 def process_file(file_name: str):
-    scene_list = detect(file_name, AdaptiveDetector())
-    folder_name = file_name.split('.')[0]
-    split_video_ffmpeg(file_name, scene_list, output_file_template=folder_name + '/$VIDEO_NAME-Scene-$SCENE_NUMBER.')
+    
