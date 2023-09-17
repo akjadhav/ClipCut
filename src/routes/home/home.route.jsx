ae7ac75100cc99e525a179874aa042ca2cbf6b0d
Mehul Rao <mehulrao@gmail.com>
2023-09-16 22:29:21 -0400

    fastapi (#1)

--- src/routes/home/home.route.jsx
+++ src/routes/home/home.route.jsx
@@ -1,102 +1,107 @@
-import { useState, useRef } from 'react';
-import './home.styles.scss';
+import { useState, useRef } from "react";
+import "./home.styles.scss";
 
 const HomeRoute = () => {
-  const [video, setVideo] = useState(null);
-  const [highlight, setHighlight] = useState(false);
-  const [activeTab, setActiveTab] = useState('upload'); // Possible values: 'upload', 'record'
-  const [recording, setRecording] = useState(false);
-  const videoRef = useRef(null);
-  const mediaRecorderRef = useRef(null);
+    const [video, setVideo] = useState(null);
+    const [highlight, setHighlight] = useState(false);
+    const [activeTab, setActiveTab] = useState("upload"); // Possible values: 'upload', 'record'
+    const [recording, setRecording] = useState(false);
+    const videoRef = useRef(null);
+    const mediaRecorderRef = useRef(null);
 
-  const handleVideoChange = (e) => {
-    const file = e.target.files[0] || e.dataTransfer.files[0];
-    if (file && file.type.startsWith('video/')) {
-      setVideo(file);
-    } else {
-      alert('Please upload a valid video file');
-      setVideo(null);
-    }
-  };
-
-  const handleSubmit = async () => {
-    if (!video) return;
-
-    const formData = new FormData();
-    formData.append('video', video);
-
-    try {
-      const response = await fetch('YOUR_BACKEND_ENDPOINT', {
-        method: 'POST',
-        body: formData,
-      });
-
-      if (response.ok) {
-        alert('Video uploaded successfully');
-      } else {
-        alert('Error uploading video');
-      }
-    } catch (error) {
-      alert('Error: ' + error);
-    }
-  };
-
-  const onDragOver = (e) => {
-    e.preventDefault();
-    setHighlight(true);
-  };
-
-  const onDragLeave = () => {
-    setHighlight(false);
-  };
-
-  const onDrop = (e) => {
-    e.preventDefault();
-    handleVideoChange(e);
-    setHighlight(false);
-  };
-
-  const handleRecord = async () => {
-    if (recording) {
-      // Stopping the mediaRecorder
-      mediaRecorderRef.current.stop();
-
-      // Stopping each track in the stream
-      videoRef.current.srcObject.getTracks().forEach((track) => track.stop());
-      videoRef.current.srcObject = null;
-
-      setRecording(false);
-      return;
-    }
-
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-
-      mediaRecorderRef.current = new MediaRecorder(stream);
-      let chunks = []; // Array to store recorded video data
-
-      mediaRecorderRef.current.ondataavailable = (event) => {
-        if (event.data.size > 0) {
-          chunks.push(event.data);
+    const handleVideoChange = (e) => {
+        const file = e.target.files[0] || e.dataTransfer.files[0];
+        if (file && file.type.startsWith("video/")) {
+            setVideo(file);
+        } else {
+            alert("Please upload a valid video file");
+            setVideo(null);
         }
-      };
+    };
 
-      mediaRecorderRef.current.onstop = () => {
-        // Convert recorded chunks to a single blob
-        const blob = new Blob(chunks, { type: 'video/webm' });
-        videoRef.current.src = URL.createObjectURL(blob);
-        chunks = []; // Clear the chunks for next recording
-      };
+    const handleSubmit = async () => {
+        if (!video) return;
 
-      videoRef.current.srcObject = stream;
-      videoRef.current.play();
+        const formData = new FormData();
+        formData.append("file", video);
 
-      mediaRecorderRef.current.start();
-      setRecording(true);
-    } catch (error) {
-      console.error('Error acquiring media stream:', error);
-    }
-  };
+        try {
+            const response = await fetch("http://127.0.0.1:8000/uploadfile/", {
+                withCredentials: true,
+                method: "POST",
+                body: formData,
+            });
+
+            if (response.ok) {
+                alert("Video uploaded successfully");
+            } else {
+                alert("Error uploading video");
+            }
+        } catch (error) {
+            alert("Error: " + error);
+        }
+    };
+
+    const onDragOver = (e) => {
+        e.preventDefault();
+        setHighlight(true);
+    };
+
+    const onDragLeave = () => {
+        setHighlight(false);
+    };
+
+    const onDrop = (e) => {
+        e.preventDefault();
+        handleVideoChange(e);
+        setHighlight(false);
+    };
+
+    const handleRecord = async () => {
+        if (recording) {
+            // Stopping the mediaRecorder
+            mediaRecorderRef.current.stop();
+
+            // Stopping each track in the stream
+            videoRef.current.srcObject
+                .getTracks()
+                .forEach((track) => track.stop());
+            videoRef.current.srcObject = null;
+
+            setRecording(false);
+            return;
+        }
+
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({
+                video: true,
+            });
+
+            mediaRecorderRef.current = new MediaRecorder(stream);
+            let chunks = []; // Array to store recorded video data
+
+            mediaRecorderRef.current.ondataavailable = (event) => {
+                if (event.data.size > 0) {
+                    chunks.push(event.data);
+                }
+            };
+
+            mediaRecorderRef.current.onstop = () => {
+                // Convert recorded chunks to a single blob
+                const blob = new Blob(chunks, { type: "video/webm" });
+                videoRef.current.src = URL.createObjectURL(blob);
+                chunks = []; // Clear the chunks for next recording
+            };
+
+            videoRef.current.srcObject = stream;
+            videoRef.current.play();
+
+            mediaRecorderRef.current.start();
+            setRecording(true);
+        } catch (error) {
+            console.error("Error acquiring media stream:", error);
+        }
+    };
 
   return (
     <>
