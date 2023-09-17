4369948b75313851a28fcf8e6726d599afe0c527
Ameya Jadhav <jadhavameyak@gmail.com>
2023-09-16 20:39:06 -0400

    misc

--- src/routes/home/home.route.jsx
+++ src/routes/home/home.route.jsx
@@ -99,54 +99,60 @@ const HomeRoute = () => {
   };
 
   return (
-    <div className='video-upload-container'>
-      <div className='tab-bar'>
-        <button
-          onClick={() => setActiveTab('upload')}
-          className={activeTab === 'upload' ? 'active' : ''}>
-          Upload
-        </button>
-        <button
-          onClick={() => setActiveTab('record')}
-          className={activeTab === 'record' ? 'active' : ''}>
-          Record
-        </button>
-      </div>
+    <>
+      <div className='app-container'>
+        <h1 className='product-title'>ClipCut</h1>
 
-      {activeTab === 'upload' ? (
-        <div className='upload-container'>
-          <div
-            className={`drop-zone ${highlight ? 'highlight' : ''}`}
-            onDragOver={onDragOver}
-            onDragLeave={onDragLeave}
-            onDrop={onDrop}>
-            Drag & Drop or Click to Upload
-            <input
-              type='file'
-              accept='video/*'
-              onChange={handleVideoChange}
-              className='file-input'
-            />
+        <div className='video-upload-container'>
+          <div className='tab-bar'>
+            <button
+              onClick={() => setActiveTab('upload')}
+              className={activeTab === 'upload' ? 'active' : ''}>
+              Upload
+            </button>
+            <button
+              onClick={() => setActiveTab('record')}
+              className={activeTab === 'record' ? 'active' : ''}>
+              Record
+            </button>
           </div>
-          <button
-            onClick={handleSubmit}
-            disabled={!video}>
-            Upload
-          </button>
+
+          {activeTab === 'upload' ? (
+            <div className='upload-container'>
+              <div
+                className={`drop-zone ${highlight ? 'highlight' : ''}`}
+                onDragOver={onDragOver}
+                onDragLeave={onDragLeave}
+                onDrop={onDrop}>
+                Drag & Drop or Click to Upload
+                <input
+                  type='file'
+                  accept='video/*'
+                  onChange={handleVideoChange}
+                  className='file-input'
+                />
+              </div>
+              <button
+                onClick={handleSubmit}
+                disabled={!video}>
+                Upload
+              </button>
+            </div>
+          ) : (
+            <div className='record-container'>
+              <video
+                ref={videoRef}
+                width='320'
+                height='240'
+                controls></video>
+              <button onClick={handleRecord}>
+                {recording ? 'Stop Recording' : 'Start Recording'}
+              </button>
+            </div>
+          )}
         </div>
-      ) : (
-        <div className='record-container'>
-          <video
-            ref={videoRef}
-            width='320'
-            height='240'
-            controls></video>
-          <button onClick={handleRecord}>
-            {recording ? 'Stop Recording' : 'Start Recording'}
-          </button>
-        </div>
-      )}
-    </div>
+      </div>
+    </>
   );
 };
 
